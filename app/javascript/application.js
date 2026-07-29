d6a8c0df8c10a9e744cf6e2ceef4505487ee2332
joeamroo <tempstarz2001@gmail.com>
2026-07-28 21:34:18 -0500

    QA fixes: entity-safe hashtags, form resets, pluralization, celebrant filter

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Clear inline forms (comments, DMs) after a successful Turbo submission
+document.addEventListener("turbo:submit-end", (event) => {
+  if (event.detail.success && event.target.querySelector("input[type=text], textarea")) {
+    event.target.reset()
+  }
+})
